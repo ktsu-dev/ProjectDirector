236a679a50a4f71639561da08cc96410355aa18c
Matthew Edmondson <matthew.edmondson@gmail.com>
2025-01-03 12:23:41 +1100

    Add documentation comments

--- ProjectDirector/AzureDevOpsRepository.cs
+++ ProjectDirector/AzureDevOpsRepository.cs
@@ -2,13 +2,36 @@ namespace ktsu.ProjectDirector;
 
 using ktsu.StrongStrings;
 
+/// <summary>
+/// Represents the name of an Azure DevOps organization.
+/// </summary>
 public sealed record class AzureDevOpsOrganizationName : StrongStringAbstract<AzureDevOpsOrganizationName> { }
+/// <summary>
+/// Represents the name of an Azure DevOps project.
+/// </summary>
 public sealed record class AzureDevOpsProjectName : StrongStringAbstract<AzureDevOpsProjectName> { }
+/// <summary>
+/// Represents the name of an Azure DevOps repository.
+/// </summary>
 public sealed record class AzureDevOpsRepoName : StrongStringAbstract<AzureDevOpsRepoName> { }
 
+/// <summary>
+/// Represents an Azure DevOps repository with organization name, project name, and repository name.
+/// </summary>
 public sealed class AzureDevOpsRepository : GitRepository
 {
+	/// <summary>
+	/// Gets or sets the name of the Azure DevOps organization.
+	/// </summary>
 	public AzureDevOpsOrganizationName OrganizationName { get; set; } = new();
+
+	/// <summary>
+	/// Gets or sets the name of the Azure DevOps project.
+	/// </summary>
 	public AzureDevOpsProjectName ProjectName { get; set; } = new();
+
+	/// <summary>
+	/// Gets or sets the name of the Azure DevOps repository.
+	/// </summary>
 	public AzureDevOpsRepoName RepoName { get; set; } = new();
 }
